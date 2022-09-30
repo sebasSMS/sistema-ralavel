0906df31dd38d2abc560fef07246d14f12785be1
Montes Sierra <se.montes@solvoglobal.local>
2022-09-30 16:57:27 -0500

    solucionando problema general clase 9 reinicio de codigo

--- database/migrations/2022_09_30_205048_create_cursos_table.php
+++ database/migrations/2022_09_30_205048_create_cursos_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddAvatarToUsersTable extends Migration
+class CreateCursosTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,8 +13,11 @@ class AddAvatarToUsersTable extends Migration
      */
     public function up()
     {
-        Schema::table('users', function (Blueprint $table) {
-            $table->string('avatar')->nullable();
+        Schema::create('cursos', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->text('descripcion');
+            $table->timestamps();
         });
     }
 
@@ -25,8 +28,6 @@ class AddAvatarToUsersTable extends Migration
      */
     public function down()
     {
-        Schema::table('users', function (Blueprint $table) {
-           $table->string('avatar')->nullable();
-        });
+        Schema::dropIfExists('cursos');
     }
 }
